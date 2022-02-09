c324d264b6def416fe4e9a8c9d046663aababceb
Krishna_ Lanka Sai (lankrishna@deloitte.com) <lankrishna@your.hostname.com>
2022-02-10 00:15:05 +0530

    Header Section containing top bar and navigator is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "./component/Header";
-import FirstTab from "./container/FirstTab";
-import SecondTab from "./container/SecondTab";
-import ThirdTab from "./container/ThirdTab";
-import ForthTab from "./container/ForthTab";
+import Header from "./component/Header/Header";
+
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const App = () => {
@@ -13,13 +10,8 @@ const App = () => {
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path="/" component={FirstTab}></Route>
-          <Route path="/firstTab" exact component={FirstTab} />
-          <Route path="/secondTab" component={SecondTab} />
-          <Route path="/thirdTab" component={ThirdTab} />
-          <Route path="/fourthTab" component={ForthTab} />
-        </Switch>
+
+
       </div>
     </BrowserRouter>
   );
---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Header.css";
+
+
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="header-title"> 
+      
+          <div style={{ flexGrow: '1' }}>
+          <Link className='top-bar-links' to="/">
+            <img src="Union.png" alt="/" /><span style={{ fontWeight: 'lighter'}}>XTREME</span><span style={{ fontWeight:'bolder' }}>CARS</span>
+          </Link>
+          </div>
+          <div style={{ flexGrow: '2' }} className='top-bar-center'>
+          <div className='top-bar-links' >
+            NEW CARS
+          </div>
+          <div className='top-bar-links' >
+            USED CARS
+          </div>
+          </div>
+          <div style={{ flexGrow: '1' }}>
+          <Link className='top-bar-links' to="/">
+            MY PROFILE
+          </Link>
+          </div>
+      
+      </div>
+      <div className="tab-items-container-main">
+      <div className="tab-items-container">
+      <Link className="tab-item" to="/firstTab">
+            SEDAN
+          </Link>
+          <Link className="tab-item" to="/secondTab">
+            SUV
+          </Link>
+          <Link className="tab-item" to="/thirdTab">
+            HATCHBACK
+          </Link>
+          <Link className="tab-item" to="/fourthTab">
+            COUPE
+          </Link>
+          
+      </div>
+      
+      <div className='type' style={{ color:'white', fontSize:'15px' }}>BODY TYPE</div>
+      </div>
+      <div className="divider"/>
+
+      
+    </div>
+  )
+}
+
+export default Header
