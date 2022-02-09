7e55ee13d8ade48e5beed7676195fdd0babcdc15
Krishna_ Lanka Sai (lankrishna@deloitte.com) <lankrishna@your.hostname.com>
2022-02-10 02:40:54 +0530

    Completed search bar

--- src/component/Main Page/MainPage.tsx
+++ src/component/Main Page/MainPage.tsx
@@ -7,7 +7,12 @@ const MainPage = () => {
     <div className="main-container">
         <img src={ MainBkg } alt=":("  />
         <div className="main-title">FIND YOUR DREAM CAR</div>
-        <div className="search-bar"><input type="text" placeholder='Enter car name'/><button>Search</button></div>
+        <div className="search-bar-container">
+            <div className="search-bar">
+            <input className="search-box" type="text" placeholder='Enter car name'/>
+            <button type='submit' className='search-button'>Search</button>
+            </div>
+        </div>
 
     </div>
   )
