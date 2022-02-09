b15c68454efe93af981f1dedc95d611b42670d44
Krishna_ Lanka Sai (lankrishna@deloitte.com) <lankrishna@your.hostname.com>
2022-02-10 01:29:36 +0530

    Added main title on the page

--- src/component/Main Page/MainPage.tsx
+++ src/component/Main Page/MainPage.tsx
@@ -5,8 +5,10 @@ import MainBkg from "./main.jpg";
 const MainPage = () => {
   return (
     <div className="main-container">
+        <img src={ MainBkg } alt=":("  />
+        <div className="main-title">FIND YOUR DREAM CAR</div>
+        <div className="search-bar">Search Bar</div>
 
-        <img src={ MainBkg } alt=""  />
     </div>
   )
 }
